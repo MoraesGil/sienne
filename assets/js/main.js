78c38ff31640a948833b5d100ed7fb3960d3b480
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-11-06 15:40:48 -0200

    finhed nav bar home slider Team

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+  $('.equipe').slick({
+    dots: true,
+    infinite: true,
+    adaptiveHeight: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});

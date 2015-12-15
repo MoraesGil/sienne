f8ca2881113cb0769550ec66b4e28d88dc0012af
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-12-15 08:48:25 -0200

    Html Puro 100%

--- assets/js/directives.js
+++
@@ -1,7 +0,0 @@
-app.directive('prettyp', function(){
-  return function(scope, element, attrs) {
-    $("[rel^='prettyPhoto']").prettyPhoto({deeplinking: false, social_tools: false});
-  }
-});
-
- 
